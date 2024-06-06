6aa5e057b12e9395de224be51c1e3203bcdeec6b
Nodar Belashvili <nodarbelashvili@gmail.com>
2024-06-06 12:59:16 +0400

    retrofit setup

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "0153549ec19545d7b3e5d8656dc6ddfd"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
